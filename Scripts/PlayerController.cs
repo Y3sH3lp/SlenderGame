d8d9245a73cd26450630472f6d397a8cab234155
Y3sH3lp <agrince18@gmail.com>
2024-12-22 10:03:00 +0200

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int booksCollected = 0; 
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Book"))
-        {
-            booksCollected++; 
-            Destroy(other.gameObject); 
-
-            
-            if (NewBehaviourScript.instance != null)
-            {
-                NewBehaviourScript.instance.UpdateEnemyAttributes(booksCollected);
-            }
-        }
-    }
+    
 }
